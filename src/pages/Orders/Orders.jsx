b3e513b14e444f99e0ec14e7dbb14f89e089dd8e
Praveshkacode <pravesh182003@gmail.com>
2024-07-24 23:56:36 +0530

    create order update feature in order.jsx

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -14,10 +14,22 @@ const Orders = ({url}) => {
     const response = await axios.get(url+"/api/order/list");
     if(response.data.success){
       setOrders(response.data.data)
-      console.log(response.data.data);
+      // console.log(response.data.data);
     }else{
       toast.error("Error")
     }
+  }
+
+  const statusHandler = async (event,orderId)=>{
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target.value
+    })
+
+    if(response.data.success){
+      await fetchAllOrders();
+    }
+    
   }
 
   useEffect(()=>{
@@ -50,7 +62,7 @@ const Orders = ({url}) => {
             </div>
             <p>Items : {order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
