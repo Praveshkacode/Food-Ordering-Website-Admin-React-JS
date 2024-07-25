fac34985c978a73d1e8fadd659badf199cf1e58d
Praveshkacode <pravesh182003@gmail.com>
2024-07-25 17:57:35 +0530

    change the content

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from './logo.png'
+import logo2 from './logo2.jpg'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
@@ -7,6 +8,7 @@ import parcel_icon from './parcel_icon.png'
 
 export const assets ={
     logo,
+    logo2,
     add_icon,
     order_icon,
     profile_image,
